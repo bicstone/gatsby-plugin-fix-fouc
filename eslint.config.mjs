9a83e2be1ce8c454e86d6a123884e4cb700c7fbd
Takanori Oishi <takanori0014@live.jp>
2024-09-19 00:17:28 +0900

    fix: Update ESLint v8 to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import eslintConfigLove from "eslint-config-love";
+// import eslintPluginImport from "eslint-plugin-import";
+
+export default [
+  {
+    ...eslintConfigLove,
+    // TODO: eslintPluginImport
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+    ignores: [".yarn", "coverage", "node_modules", "*.js", "*.d.ts", "e2e"],
+  },
+];
